347c3e6dae51ebf2cb013f418ccdc0e08a1cc1e4
coffeemoka <takuma.fn@gmail.com>
2021-09-21 23:47:43 +0900

    Add Other files.

---
+++ src/server/index.ts
@@ -0,0 +1,18 @@
+import * as express from "express";
+import * as http from "http";
+import * as socketio from "socket.io";
+
+const app: express.Express = express.default();
+const server: http.Server = http.createServer(app);
+const io: socketio.Server = new socketio.Server(server);
+const port: number = 3000;
+
+app.use(express.static("public"));
+
+io.on("connection", (socket: socketio.Socket) => {
+    socket.on("hoge", (msg: string) => {
+        io.emit("hoge", msg);
+    });
+});
+
+server.listen(port, ()=> console.log(`start port:${port}`));
